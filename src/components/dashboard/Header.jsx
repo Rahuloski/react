7dfb21e2fd30da5a58cbd406981e56e5bbe80783
Rahuloski <rahulroychonedan@gmail.com>
2025-02-18 15:56:17 +0530

    header

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import icon from "../../assets/logo.svg";
 
 function Header() {
-  return (
-    <div>
-      <h1>hi guyss</h1>
-    </div>
-  )
+    return (
+        <div className="bg-[#000] flex justify-between items-center">
+            <div className="flex items-center gap-4 p-4">
+                <div className="w-[40px] h-[40px] mr-2">
+                    <img src={icon} alt="Logo" width={100} height={100} />
+                </div>
+                <h4 className="text-[#fff] text-[24px]">Tailwind</h4>
+            </div>
+            <div>
+                <ul className="text-[#fff] flex gap-6">
+                    <li>Docs</li>
+                    <li>Components</li>
+                    <li>Blog</li>
+                    <li>GitHub</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
-export default Header
+export default Header;
